6651b4b274456681ad69f7dbe3c2ab5f5c361bc5
tukiyo3 <tukiyo3@gmail.com>
2016-06-20 08:37:31 +0900

    update

--- nadm.py
+++ nadm.py
@@ -4,24 +4,25 @@ import sys
 dir = '/etc/nagios3/conf.d'
 dir = '.'
 #------------------------------------------------------
-def help(emsg=""):
-  if emsg != "" : print( "Error: "+emsg)
-  print("usage: "+sys.argv[0]+" group host ip")
-  list()
-  sys.exit(1)
+def help(emsg):
+    print(emsg)
+    print("usage: {} group host ip").format(sys.argv[0])
+    list()
 
 def list():
-  for f in os.listdir(dir) :
-      if not '_nagios2.cfg' in f:
-          print "\t"+dir +"/"+ f
+    for f in os.listdir(dir) :
+        if '.cfg' in f and '_nagios2' not in f:
+            print "\t{dir}/{file}".format(dir=dir,file=f)
 
 def add(group, host, ip):
-  write(define_host(group, host, ip), dir+"/host_" + group + ".cfg")
-  write(define_chek(group, host, ip), dir+"/host_" + group + "_check.cfg")
-  list()
+    write(define_host(group, host, ip)
+        ,"{dir}/host_{group}.cfg".format(dir=dir, group=group))
+    write(define_chek(group, host, ip)
+        ,"{dir}/host_{group}_check.cfg".format(dir=dir, group=group))
+    list()
 
 def define_host(group, host, ip):
-  form = """# {host} ({ip})"
+    form = """# {host} ({ip})"
 define host{{
     use        generic-host
     host_name  {group}-{host}
@@ -30,12 +31,12 @@ define host{{
 #   check_command return-ok
     }}
 """
-  text = form.format(group=group, host=host, ip=ip)
-  return text
+    text = form.format(group=group, host=host, ip=ip)
+    return text
 
 # see: /usr/lib/nagios/plugins/
 def define_chek(group, host, ip):
-  form = """## {host} ({ip})
+    form = """## {host} ({ip})
 #define service{{
 #    service_description Alive
 #    host_name           {group}-{host}
@@ -43,16 +44,18 @@ def define_chek(group, host, ip):
 #    check_command       check-host-alive
 #    }}
 """
-  text = form.format(group=group, host=host, ip=ip)
-  return text
+    text = form.format(group=group, host=host, ip=ip)
+    return text
 
 def write(txt, fname):
-  fp = open(fname, 'a+')
-  fp.write(txt)
-  fp.close()
+    fp = open(fname, 'a+')
+    fp.write(txt)
+    fp.close()
 #------------------------------------------------------
 if __name__ == '__main__' :
-  if len(sys.argv) == 1 : help()
-  elif len(sys.argv) != 4 : help("more arguments for add command")
-  add(sys.argv[1], sys.argv[2], sys.argv[3])
-  print("check: nagios3 -v /etc/nagios3/nagios.cfg && service nagios3 restart")
+    if len(sys.argv) != 4:
+        help('[ERROR] specify argument is wrong.')
+        sys.exit(1)
+
+    add(sys.argv[1], sys.argv[2], sys.argv[3])
+    print("check: nagios3 -v /etc/nagios3/nagios.cfg && service nagios3 restart")
